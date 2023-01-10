33f66bb6f7279e2665c92a78d4fc1d4255a1c263
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-01-10 11:26:07 +0100

    programmed ending of game when lost or when won

--- script.js
+++ script.js
@@ -14,6 +14,7 @@
     let randomWord = array[Math.floor(Math.random()*array.length)].split('') //random word from the array and splitted into single letters array
     const alphabet = [...Array(26)].map((ele,i) => ele = String.fromCharCode(i+97)) 
     const inputs = [...Array(randomWord.length).fill(' _ ')]
+    const allInputs =[]
     const letterGrid = document.getElementById('letter-grid')
     const pictures = [...Array(7)].map((ele,i) => ele = './images/hangman'+ i + '.jpg')
 
@@ -35,7 +36,8 @@
     alphabet.forEach((ele, i) => {
                     
         document.getElementsByClassName("letter")[i].addEventListener("click", (event) => {
-            if(inputs.some(input => input === ' _ ' ) ){
+            console.log("all inputs: ", allInputs)
+            if(inputs.some(input => input === ' _ ' ) && lives<7 ){
                 inputs.forEach(ele => {
                     hiddenWord.innerHTML =``
                 }) 
@@ -44,22 +46,30 @@
                 inputs.forEach(inp => {
                     hiddenWord.innerHTML +=`<span class="hidden-letter">${inp}</span>`
                 }) 
-                console.log('user inputs : ', inputs) 
-    
-                //if the clicked letter is also in the random word color yellow else red
-                if(randomWord.includes(event.target.innerHTML))
-                {
-                    document.getElementsByClassName("letter")[i].style.backgroundColor="yellow" 
-                    console.log(score++ ) 
-                }  
-                else {
-                    document.getElementsByClassName("letter")[i].style.backgroundColor="red" 
-                    document.getElementById('image').innerHTML = `<img src = ${pictures[lives]}> </img>` 
-                    lives++
+                console.log('user correct inputs : ', inputs) 
+
+                if(!allInputs.includes(event.target.innerHTML)){
+                    allInputs.push(event.target.innerHTML)
                     
-                }                    
-                inputs.some(input => input === ' _ ' ) ? 0: alert("victory")
-            } else console.log("the game is finished")
+                    console.log("checked that the letter was not yet there", event.target.innerHTML)
+
+                    //if the clicked letter is also in the random word color yellow else red
+                    if(randomWord.includes(event.target.innerHTML))
+                    {
+                        document.getElementsByClassName("letter")[i].style.backgroundColor="yellow" 
+                        score++
+                        console.log(`your score is ${score}` ) 
+                    }  
+                    else {
+                        document.getElementsByClassName("letter")[i].style.backgroundColor="red" 
+                        document.getElementById('image').innerHTML = `<img src = ${pictures[lives]}> </img>` 
+                        lives++
+                        console.log(`you used ${lives} live(s)`)
+                        lives === 7 ? alert("You lost the game"):0
+                    }                   
+                }
+                (inputs.some(input => input === ' _ ' )) ? 0: alert("victory")
+            } else alert("the game is finished")
         }) 
    
 
